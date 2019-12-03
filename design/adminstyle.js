ed71490b7a1b442b9dadf338c1fa1eb7448b180b
Issac-Kim <kimi6@ri.edu>
2019-12-03 03:32:08 -0500

    admin base form styling and js for style

---
+++ design/adminstyle.js
@@ -0,0 +1,43 @@
+$("#create-team-btn").click(function(){
+    $("#create-team-btn").addClass("active");
+    $("#add-team-btn").removeClass("active");
+    $("#edit-team-btn").removeClass("active");
+    $("#upload-photo-btn").removeClass("active");
+    $("#team-create").removeClass("hidden");
+    $("#add-team").addClass("hidden");
+    $("#edit-team").addClass("hidden");
+    $("#upload-photo").addClass("hidden");
+});
+
+$("#add-team-btn").click(function(){
+    $("#create-team-btn").removeClass("active");
+    $("#add-team-btn").addClass("active");
+    $("#edit-team-btn").removeClass("active");
+    $("#upload-photo-btn").removeClass("active");
+    $("#team-create").addClass("hidden");
+    $("#add-team").removeClass("hidden");
+    $("#edit-team").addClass("hidden");
+    $("#upload-photo").addClass("hidden");
+});
+
+$("#edit-team-btn").click(function(){
+    $("#create-team-btn").removeClass("active");
+    $("#add-team-btn").removeClass("active");
+    $("#edit-team-btn").addClass("active");
+    $("#upload-photo-btn").removeClass("active");
+    $("#team-create").addClass("hidden");
+    $("#add-team").addClass("hidden");
+    $("#edit-team").removeClass("hidden");
+    $("#upload-photo").addClass("hidden");
+});
+
+$("#upload-photo-btn").click(function(){
+    $("#create-team-btn").removeClass("active");
+    $("#add-team-btn").removeClass("active");
+    $("#edit-team-btn").removeClass("active");
+    $("#upload-photo-btn").addClass("active");
+    $("#team-create").addClass("hidden");
+    $("#add-team").addClass("hidden");
+    $("#edit-team").addClass("hidden");
+    $("#upload-photo").removeClass("hidden");
+});
