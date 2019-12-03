2fcf4642d4c5c054657cfd3e1cb80f1f17c0911b
Issac-Kim <kimi6@ri.edu>
2019-12-03 04:06:27 -0500

    dragging stuff for plays

--- design/playbook.js
+++ design/playbook.js
@@ -1,20 +1,85 @@
+
+var dragging = false; 
+var rollover = false; 
+
+var x, y, w, h;       
+var offsetX, offsetY;    
+
 function setup(){
-    let canvas = createCanvas(400, 400);
+    let canvas = createCanvas(600, 400);
     canvas.parent("playbook-container");
     // set the background to the image 
     bg = loadImage("resources/pictures/field.jpg");
     // used to set the number of each type of icon available to the user 
     let players = 10;
     let opponents = 10;
-    let new_player = document.getElementById("player");
-    let new_opponent = document.getElementById("opponent");
+    player = createButton('Add Player');
+    player.position(0, 0);
+    player.mousePressed(addP);
+    opponent = createButton('Add Opponent');
+    opponent.position(150,0);
+    opponent.mousePresses(addO);
+    // Starting location
+    x = 100;
+    y = 100;
+    // Dimensions
+    w = 75;
+    h = 50;
 }
-
 
 function draw(){
     background(bg);
+      // Is mouse over object
+  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    rollover = true;
+  } 
+  else {
+    rollover = false;
+  }
+  
+  // Adjust location if being dragged
+  if (dragging) {
+    x = mouseX + offsetX;
+    y = mouseY + offsetY;
+  }
 
+  stroke(0);
+  // Different fill based on state
+  if (dragging) {
+    fill (50);
+  } else if (rollover) {
+    fill(100);
+  } else {
+    fill(175, 200);
+  }
+  rect(x, y, w, h);
 }
+
+
+function mousePressed() {
+  // Did I click on the rectangle?
+  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    dragging = true;
+    // If so, keep track of relative location of click to corner of rectangle
+    offsetX = x-mouseX;
+    offsetY = y-mouseY;
+  }
+}
+
+function mouseReleased() {
+  // Quit dragging
+  dragging = false;
+}
+
+function addP(){
+    let val = 1;
+};
+
+function addO(){
+    let val = 1;
+}
+
+
 
 
 
