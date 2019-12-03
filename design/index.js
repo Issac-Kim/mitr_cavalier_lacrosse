d8ffab9849fc7e33b71756dfdec4a166dfbdd7b2
Ronna Min <ronnamin@gmail.com>
2019-12-03 02:46:05 -0500

    Comments and fixing small details

--- design/index.js
+++ design/index.js
@@ -1,12 +1,11 @@
-
-
+// navbar scroll color animation
 $(document).scroll(function(){
     var pos = $(document).scrollTop();
     var home_content = $("#home-content").offset().top;
     if(pos >= home_content){
-        $("#over-background").css("background-color", "black");
+        $("#over-background").css("background-color", "black"); // make navbar background black when scrolling down
     }else{
-        $("#over-background").css("background-color", "transparent");
+        $("#over-background").css("background-color", "transparent");; // reset navbar to transparent
     }
 });
 
