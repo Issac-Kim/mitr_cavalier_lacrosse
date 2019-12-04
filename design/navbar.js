cb65161c88679c9950afd3094ead93b082c69628
Issac-Kim <kimi6@ri.edu>
2019-12-03 22:21:35 -0500

    update the home page, bug with the navbar scrolling

--- design/navbar.js
+++ design/navbar.js
@@ -1,11 +1,12 @@
 // navbar scroll color animation
-$(document).scroll(function(){
-    var pos = $(document).scrollTop();
+$(this).scroll(function(){
+    var pos = $(this).scrollTop();
     var home_content = $("#home-content").offset().top;
+
     if(pos >= home_content){
         $("#over-background").css("background-color", "#154890"); // make navbar background black when scrolling down
     }else{
-        $("#over-background").css("background-color", "transparent");; // reset navbar to transparent
+        $("#over-background").css("background-color", "transparent"); // reset navbar to transparent
     }
 });
 
